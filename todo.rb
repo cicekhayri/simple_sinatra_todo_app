59ab9344a3836e45504a74159202002419d7a900
Hayri <hello@hoshilab.com>
2014-09-10 21:06:29 +0200

    indenting fixes

--- todo.rb
+++ todo.rb
@@ -4,40 +4,40 @@ require 'data_mapper'
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/default.db")
 
 class Note
-	include DataMapper::Resource
-	property :id, Serial
-	property :content, Text, :required => true
-	property :created_at, DateTime
-	property :updated_at, DateTime
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :created_at, DateTime
+  property :updated_at, DateTime
 end
 
 DataMapper.finalize.auto_upgrade!
 
 get '/' do
-	@notes = Note.all :order => :id.desc
-	erb :index
+  @notes = Note.all :order => :id.desc
+  erb :index
 end
 
 post '/' do
-	note = Note.new
-	note.content = params[:content]
-	note.created_at = Time.now
-	note.updated_at = Time.now
-	note.save
-	redirect '/'
+  note = Note.new
+  note.content = params[:content]
+  note.created_at = Time.now
+  note.updated_at = Time.now
+  note.save
+  redirect '/'
 end
 
 get '/:id' do
-	@note = Note.get params[:id]
-	erb :edit
+  @note = Note.get params[:id]
+  erb :edit
 end
 
 put '/:id' do
-	note = Note.get params[:id]
-	note.content = params[:content]
-	note.updated_at = Time.now
-	note.save
-	redirect '/'
+  note = Note.get params[:id]
+  note.content = params[:content]
+  note.updated_at = Time.now
+  note.save
+  redirect '/'
 end
 
 get '/:id/delete' do
